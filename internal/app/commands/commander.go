fa81af5a240dbc55c545f94a27cad4ced6b35543
edikgoose <edikgoose@gmail.com>
2022-06-11 16:49:43 +0300

    Add recover

--- internal/app/commands/commander.go
+++ internal/app/commands/commander.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"github.com/edikgoose/bot/internal/service/product"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
 )
 
 var registeredCommands = map[string]func(c *Commander, msg *tgbotapi.Message){}
@@ -23,17 +24,15 @@ func NewCommander(
 }
 
 func (c *Commander) HandleUpdate(update tgbotapi.Update) {
+	defer func() {
+		if panicValue := recover(); panicValue != nil {
+			log.Printf("recoverd from panic: %v", panicValue)
+		}
+	}()
+
 	if update.Message == nil {
 		return
 	}
-
-	// Вторая версия как это сделать без свичей
-	//command, ok := registeredCommands[update.Message.Command()]
-	//if ok {
-	//	command(c, update.Message)
-	//} else {
-	//	c.Default(update.Message)
-	//}
 
 	switch update.Message.Command() {
 	case "help":
--- internal/service/product/service.go
+++ internal/service/product/service.go
@@ -9,3 +9,8 @@ func NewService() *Service {
 func (s *Service) List() []Product {
 	return allProducts
 }
+
+func (s *Service) Get(idx int) (*Product, error) {
+	// TODO: сделать проверку и в случае невалидного индекса возвращать ошибку (обработать её тоже не забыть)
+	return &allProducts[idx], nil
+}
